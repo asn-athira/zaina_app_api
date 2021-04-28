2bc9d9769761ceffdabff69fbb78e0255e89e9f4
asn-athira <athira@rightsolution.ae>
2021-04-28 17:22:25 +0530

    modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,16 +14,18 @@ Offer.create!([{
   phone: "+971 356 657",
   description: "offering effecient cleaning with 50%"
 	},
+	{
 	offer_name: "offer_2",
   location: "40% discount in sofa dry cleaning",
   phone: "+971 356 567",
   description: "offering effecient cleaning with 40%"
 	},
+	{
 	offer_name: "offer_3",
   location: "30% discount in sofa ironing",
   phone: "+971 232 657",
   description: "offering effecient cleaning with 30%"
-	},
+	}
 
 ])
 
@@ -35,12 +37,14 @@ Service.create!([{
   service_name: "Wash & dry",
   description: "We use Imported Machines and Eco Friendly detergents to take care of your fabric . . ."
 },
+{
   service_name: "Steam ironing",
   description: "We curve your fabric with Steam Iron to ensure no more wrinkles upon them . . ."
 },
+{
   service_name: "Dry cleaning",
   description: "We offers best in class dry cleaning service retaining the freshness of your fabric . . ."
-},
+}
 
 ])
 
